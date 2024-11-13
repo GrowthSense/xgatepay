be23d53970421d9b0db1c9dd33497105766b9123
macmhonyera <macmhonyera@gmail.com>
2024-11-13 09:32:59 +0200

    get transactions done

--- src/wallet/wallet.service.ts
+++ src/wallet/wallet.service.ts
@@ -1,6 +1,5 @@
 
 import { BadRequestException, Injectable } from '@nestjs/common';
-import { InjectRepository } from '@nestjs/typeorm';
 import StellarSdk,{ Keypair } from '@stellar/stellar-sdk';
 
 
